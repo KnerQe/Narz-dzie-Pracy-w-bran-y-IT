f40751b397ad8d127a3579c35ef0ce56dbca3895
KnerQe <kamil.koczwara.biz@gmail.com>
2025-04-27 14:33:04 +0200

    Komendy

---
+++ plik.sh
@@ -0,0 +1,12 @@
+ 91  git commit -m "Opis commitu"
+   92  git remote add origin https://github.com/KnerQe/Narz-dzie-Pracy-w-bran-y-IT
+   93  git push --set-upstream origin master
+   94  git remote add origin https://github.com/KnerQe/Narz-dzie-Pracy-w-bran-y-IT.git
+   95  git push --set-upstream origin master
+   96  git pull origin master
+   97  git push --set-upstream origin master
+   98  git branch -M main
+   99  git push -u origin main
+  100  git push -u origin main
+  101  git push -u origin main
+  102  git push -u origin main
